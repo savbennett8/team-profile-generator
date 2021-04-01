16169da05ff6e9f0d8595a5a79fe2f729832ed48
Savannah Bennett <savvy.bennett8@gmail.com>
2021-03-31 20:05:09 -0600

    test / pass employee object

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toStrictEqual(expect.any(Number));
+    expect(employee.email).toStrictEqual(expect.any(String));
+});
