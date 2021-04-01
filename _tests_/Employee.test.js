7f4475b625a5b3ddfe3ae0420a8b6aa1612df149
Savannah Bennett <savvy.bennett8@gmail.com>
2021-03-31 20:19:33 -0600

    test / pass getName method

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -8,3 +8,9 @@ test('creates an employee object', () => {
     expect(employee.id).toStrictEqual(expect.any(Number));
     expect(employee.email).toStrictEqual(expect.any(String));
 });
+
+test('gets employees name', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getName()).toStrictEqual(expect.any(String));
+});
