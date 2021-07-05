bbed1c708c8e7302dbb226d79e7da88540b872be
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-05 12:40:38 -0600

    re-did employee constructor/class and rechecked tests. also a few minor changes to other files

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,36 +1,27 @@
 const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+const employee = new Employee('Erin', 'Singer', 1);
 
 test('creates an employee object', () => {
-    const employee = new Employee('Erin');
-
-    expect(employee.name).toBe('Erin');
-    expect(employee.id).toStrictEqual(expect.any(String));
-    expect(employee.email).toStrictEqual(expect.any(String));
+    expect(employee.firstName).toBe('Erin');
+    expect(employee.lastName).toBe('Singer');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('@'));
+    expect(employee.role).toBe('Employee');
 });
 
 test('gets employees name', () => {
-    const employee = new Employee('Erin');
-
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 test('get employee id number', () => {
-    const employee = new Employee('Erin');
-
     expect(employee.getId()).toEqual(expect.any(String));
 });
 
 test('get employee email', () => {
-    const employee = new Employee('Erin');
-
     expect(employee.getEmail()).toEqual(expect.any(String));
 });
 
 test('get employee role', () => {
-    const employee = new Employee('Erin');
-
-    expect(employee.getRole()).toHaveProperty('name');
-    expect(employee.getRole()).toHaveProperty('id');
-    expect(employee.getRole()).toHaveProperty('email');
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role));
 });
